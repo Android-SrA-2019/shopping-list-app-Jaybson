310539b5074dee6ba0a668528b5ced7e26a50c34
Jeremy Johnson <jeremy.b.johnson96@gmail.com>
2019-02-05 13:49:49 -0400

    Task 1 & 2 Completed
    Completed the design on the Main and Second activity. Also coded the 1 Add Item button and 10 Item buttons to go from one activity to another.

--- app/src/main/java/ca/nbcc/shoppinglist/MainActivity.java
+++ app/src/main/java/ca/nbcc/shoppinglist/MainActivity.java
@@ -1,13 +1,33 @@
 package ca.nbcc.shoppinglist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    //public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
+    public static final int TEXT_REQUEST = 1;
+    //private EditText mMessageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(LOG_TAG, "onCreate JBJ");
+
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button Click JBJ");
+        Intent intent = new Intent(this, SecondActivity.class);
+        //String message = mMessageEditText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
     }
 }
--- app/src/main/java/ca/nbcc/shoppinglist/SecondActivity.java
+++ app/src/main/java/ca/nbcc/shoppinglist/SecondActivity.java
@@ -1,13 +1,29 @@
 package ca.nbcc.shoppinglist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+    //public static final String EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE";
+    public static final int TEXT_REQUEST = 1;
+    //private EditText mMessageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    public void launchMainActivity(View view) {
+        Log.d(LOG_TAG, "Button Click JBJ");
+        Intent intent = new Intent(this, MainActivity.class);
+        //String message = mMessageEditText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
     }
 }
